f14d05f2fcd1bf062ec78ae0578582cb71195d1f
Leonid <leonigleo13@gmail.com>
2025-04-27 12:35:25 +0300

    Seven step 1.0

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import TaskFormComponent from "./view/task-form-component.js";
 import TasksBoardPresenter from "./presenter/tasks-board-presenter.js";
 import { render, RenderPosition } from "./framework/render.js";
 import TasksModel from "./model/task-model.js";
+import TasksApiService from "./tasks-api-service.js";
 
 const bodyContainer = document.querySelector(".board-app");
 const formContainer = document.querySelector(".add-task");
 const tasksBoardContainer = document.querySelector(".taskboard");
+const END_POINT = "https://680b2032d5075a76d989e8f4.mockapi.io";
 
-const tasksModel = new TasksModel();
+const tasksModel = new TasksModel({
+  tasksApiService: new TasksApiService(END_POINT),
+});
+
 const tasksBoardPresenter = new TasksBoardPresenter({
   boardContainer: tasksBoardContainer,
   tasksModel,
